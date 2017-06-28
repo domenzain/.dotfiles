d456b72c8f8a2acad89e65e6b945565f7b2ccbb5
M. Domenzain <ld@airinov.fr>
2017-06-28 12:37:47 +0200

    Remove unnecessary outer terminal

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -32,5 +32,5 @@ main = do
         , ((0, xF86XK_AudioMicMute    ), spawn "amixer -D pulse -- sset Capture toggle")
         -- Shortcuts
         , ((mod1Mask, xK_m), spawn "gnome-terminal -e mutt")
-        , ((mod1Mask, xK_e), spawn "gnome-terminal -e \"emacsclient -c\"")
+        , ((mod1Mask, xK_e), spawn "emacsclient -c")
         ]
