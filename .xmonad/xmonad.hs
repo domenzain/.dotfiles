0474b8f9ee4fdd7ce0b567fc24330f630fa4dfe5
M. Domenzain <luis.domenzain@parrot.com>
2019-08-02 17:44:22 +0200

    update xmonad configuration keys and layouts

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -7,16 +7,19 @@ import XMonad.Wallpaper.Expand
 import XMonad.Wallpaper.Find
 import XMonad.Prompt
 import XMonad.Prompt.Pass
+import XMonad.Layout
+import XMonad.Layout.Accordion
+import XMonad.Layout.Grid
 import System.Random
 import Graphics.X11.ExtraTypes.XF86
 import qualified Data.Map as M
-import System.Exit (exitWith, ExitCode(..) )
+import System.Exit (exitSuccess )
 
 setRandomWallpaper :: [String] -> IO()
 setRandomWallpaper filepaths = do
     rootPaths  <- mapM expand filepaths
     candidates <- findImages rootPaths
-    wallpaper  <- ((!!) candidates) <$> getStdRandom (randomR (0, length candidates - 1))
+    wallpaper  <- (!!) candidates <$> getStdRandom (randomR (0, length candidates - 1))
     spawn $ "feh --bg-fill " ++ wallpaper
 
 -- TODO: get $EDITOR and pass it on
@@ -30,60 +33,60 @@ rotateWS b  = do t <- findWorkspace getSortByIndex (bToDir b) NonEmptyWS 1
         bToDir False = Prev
 
 myKeys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X ())
-myKeys configDefault@(XConfig {XMonad.modMask = configModMask}) = M.fromList $
+myKeys configDefault@XConfig {XMonad.modMask = configModMask} = M.fromList
   [ ((configModMask, xK_l), spawn "slock")
   , ((configModMask, xK_Tab), rotateWS True)
   , ((configModMask .|. shiftMask, xK_Tab), rotateWS False)
     -- Media buttons
   , ((0, xF86XK_MonBrightnessUp  ), spawn "light -A 5")
   , ((0, xF86XK_MonBrightnessDown), spawn "light -U 5")
-  , ((0, xF86XK_AudioLowerVolume), spawn "amixer -D pulse -- sset Master unmute 5%-")
-  , ((0, xF86XK_AudioRaiseVolume), spawn "amixer -D pulse -- sset Master unmute 5%+")
-  , ((0, xF86XK_AudioMute       ), spawn "amixer -D pulse -- sset Master toggle")
-  , ((0, xF86XK_AudioMicMute    ), spawn "amixer -D pulse -- sset Capture toggle")
+  , ((0, xF86XK_AudioLowerVolume ), spawn "amixer -D pulse -- sset Master unmute 5%-")
+  , ((0, xF86XK_AudioRaiseVolume ), spawn "amixer -D pulse -- sset Master unmute 5%+")
+  , ((0, xF86XK_AudioMute        ), spawn "amixer -D pulse -- sset Master toggle")
+  , ((0, xF86XK_AudioMicMute     ), spawn "amixer -D pulse -- sset Capture toggle")
     -- Displays
   , ((configModMask, xK_Left), onPrevNeighbour verticalScreenOrderer view)
   , ((configModMask, xK_Right), onNextNeighbour verticalScreenOrderer view)
   , ((configModMask .|. shiftMask, xK_Left),
-      (onPrevNeighbour verticalScreenOrderer shift)
-      >> (onPrevNeighbour verticalScreenOrderer view))
+      onPrevNeighbour verticalScreenOrderer shift
+      >> onPrevNeighbour verticalScreenOrderer view)
   , ((configModMask .|. shiftMask, xK_Right),
-      (onNextNeighbour verticalScreenOrderer shift)
-      >> (onNextNeighbour verticalScreenOrderer view))
-    -- Windows
-  , ((configModMask, xK_Up), windows focusUp)
-  , ((configModMask, xK_Down), windows focusDown)
-  , ((configModMask .|. shiftMask, xK_Up), windows swapUp)
-  , ((configModMask .|. shiftMask, xK_Down), windows swapDown)
-  , ((configModMask .|. shiftMask, xK_Return), spawn $ XMonad.terminal configDefault) -- %! Launch terminal
-  , ((configModMask,               xK_p     ), spawn "dmenu_run") -- %! Launch dmenu
-  , ((configModMask .|. shiftMask, xK_p     ), spawn "gmrun") -- %! Launch gmrun
-  , ((configModMask .|. shiftMask, xK_c     ), kill) -- %! Close the focused window
+      onNextNeighbour verticalScreenOrderer shift
+      >> onNextNeighbour verticalScreenOrderer view)
+    -- Windows and programs
+  , ((configModMask, xK_Up                  ), windows focusUp)
+  , ((configModMask, xK_Down                ), windows focusDown)
+  , ((configModMask .|. shiftMask, xK_Up    ), windows swapUp)
+  , ((configModMask .|. shiftMask, xK_Down  ), windows swapDown)
+  , ((configModMask .|. shiftMask, xK_c     ), kill)
+  , ((configModMask,               xK_space ), sendMessage NextLayout)
 
-  , ((configModMask,               xK_space ), sendMessage NextLayout) -- %! Rotate through the available layout algorithms
-  , ((configModMask .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook configDefault) -- %!  Reset the layouts on the current workspace to default
+  , ((configModMask .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook configDefault)
+  , ((configModMask .|. shiftMask, xK_Return), spawn $ XMonad.terminal configDefault)
+  , ((configModMask,               xK_p     ), spawn "dmenu_run")
+  , ((configModMask .|. shiftMask, xK_p     ), spawn "gmrun")
 
-  , ((configModMask,               xK_n     ), refresh) -- %! Resize viewed windows to the correct size
+  , ((configModMask, xK_n), refresh) -- %! Resize viewed windows to the correct size
 
   -- move focus up or down the window stack
-  , ((configModMask,               xK_m     ), windows focusMaster  ) -- %! Move focus to the master window
+  , ((configModMask, xK_m), windows focusMaster)
 
   -- modifying the window order
-  , ((configModMask,               xK_Return), windows swapMaster) -- %! Swap the focused window and the master window
+  , ((configModMask, xK_Return), windows swapMaster)
 
   -- resizing the master/slave ratio
-  -- , ((configModMask,               xK_h     ), sendMessage Shrink) -- %! Shrink the master area
-  -- , ((configModMask,               xK_l     ), sendMessage Expand) -- %! Expand the master area
+  , ((configModMask, xK_comma), sendMessage Shrink)
+  , ((configModMask, xK_period), sendMessage Expand)
 
   -- floating layer support
-  , ((configModMask,               xK_t     ), withFocused $ windows . sink) -- %! Push window back into tiling
+  , ((configModMask, xK_t     ), withFocused $ windows . sink)
 
   -- increase or decrease number of windows in the master area
-  , ((configModMask              , xK_comma ), sendMessage (IncMasterN 1)) -- %! Increment the number of windows in the master area
-  , ((configModMask              , xK_period), sendMessage (IncMasterN (-1))) -- %! Deincrement the number of windows in the master area
+  , ((configModMask .|. shiftMask, xK_comma), sendMessage (IncMasterN 1))
+  , ((configModMask .|. shiftMask, xK_period), sendMessage (IncMasterN (-1)))
 
   -- quit, or restart
-  , ((configModMask .|. shiftMask, xK_q     ), io (exitWith ExitSuccess)) -- %! Quit xmonad
+  , ((configModMask .|. shiftMask, xK_q     ), io exitSuccess)  -- %! Quit xmonad
   , ((configModMask              , xK_q     ), spawn "if type xmonad; then xmonad --recompile && xmonad --restart; else xmessage xmonad not in \\$PATH: \"$PATH\"; fi") -- %! Restart xmonad
 
   , ((configModMask, xK_e), spawn editor)
@@ -92,6 +95,15 @@ myKeys configDefault@(XConfig {XMonad.modMask = configModMask}) = M.fromList $
                                      , font = "xft:Source Code Pro-9"
                                      })
   ]
+
+
+myLayout = Mirror tiled ||| Grid ||| Accordion ||| Full
+    where
+      tiled = Tall nmaster delta ratio
+      nmaster = 1
+      ratio = 2/3
+      delta = 1/100
+
 main :: IO()
 main = do
     setRandomWallpaper ["${HOME}/Pictures/Paintings/"]
@@ -103,4 +115,5 @@ main = do
     , focusedBorderColor = "#00ff00"
     , modMask = mod4Mask
     , keys = myKeys
+    , layoutHook = myLayout
     }
